1083697edd86c14726c7de3ebfceb9080b90ee87
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2026-02-21 12:51:34 +0600

    14th commit

---
+++ src/Pages/Home/Reviews/ReviewCard/ReviewCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FaQuoteLeft } from 'react-icons/fa';
+
+const ReviewCard = ({reviews}) => {
+      const {userName,userPhotoURL,date,review} = reviews
+      return (
+            <div>
+                  <div className="max-w-md mx-auto">
+      <div className="card bg-base-200 shadow-xl rounded-2xl p-6">
+        
+   
+        <FaQuoteLeft className="text-4xl text-primary opacity-40 mb-4" />
+
+ 
+        <p className="text-base-content/80 leading-relaxed">
+          {review}
+        </p>
+
+    
+        <div className="border-t border-dashed border-base-content/30 my-5"></div>
+
+
+        <div className="flex items-center gap-4">
+          
+    
+          <div className="avatar placeholder">
+            <div className="bg-primary text-primary-content rounded-full w-12">
+              <span className="text-lg font-semibold">{userPhotoURL}</span>
+            </div>
+          </div>
+
+      
+          <div>
+            <h3 className="font-semibold text-lg">{userName}</h3>
+            <p className="text-sm opacity-70">{date}</p>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+            </div>
+      );
+};
+
+export default ReviewCard;
--- src/Pages/Home/Reviews/Reviews.jsx
+++ src/Pages/Home/Reviews/Reviews.jsx
@@ -1,4 +1,7 @@
 import React, { use } from 'react';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import ReviewCard from './ReviewCard/ReviewCard';
 
 const Reviews = ({ReviewsPromise}) => {
 
@@ -6,7 +9,32 @@ const Reviews = ({ReviewsPromise}) => {
       console.log(reviews)
       return (
             <div>
-                  
+                   <>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={3}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        {
+            reviews.map(reviews=><SwiperSlide key={reviews.id}>
+           <ReviewCard reviews={reviews}></ReviewCard>
+        </SwiperSlide>)
+        }
+        
+        
+      </Swiper>
+    </>
             </div>
       );
 };
