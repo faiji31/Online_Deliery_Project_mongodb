30ebebafe3e3846009b5835e67a8ad4de3656f16
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2026-02-24 22:50:21 +0600

    22nd commit

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+
 
 const Register = () => {
+  const {register, handleSubmit , formState:{errors}} = useForm();
+const handleRegistration =(data)=>{
+  console.log('after register',data)
+
+}
   return (
     <div>
-      
+      <form onSubmit={handleSubmit(handleRegistration)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" {...register('email',{required: true})} className="input" placeholder="Email" />
+          {errors.email?.type =='required' &&  <p className='text-red-500'>Email is required</p>}
+          <label className="label">Password</label>
+          <input type="password" {...register('password',{required: true,
+          minLength:6})} className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+
+           {errors.password?.type =='required' &&  <p className='text-red-500'>Password is required</p>}
+           {
+            errors.password?.type ==='minLength' && <p className='text-red-500'>Password minimum length is 6</p>
+           }
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
     </div>
   );
 };
