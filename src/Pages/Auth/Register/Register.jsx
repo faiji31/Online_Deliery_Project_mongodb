afa8a353a05815ce088e8b1bbd2fdd41f72932e8
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2026-02-26 13:31:11 +0600

    26th commit

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import useAuth from '../../../hooks/useAuth';
 import { Link } from 'react-router';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import axios from 'axios';
 
 
 
@@ -10,10 +11,22 @@ const Register = () => {
   const { registerUser} =useAuth();
   const {register, handleSubmit , formState:{errors}} = useForm();
 const handleRegistration =(data)=>{
-  console.log('after register',data)
+  console.log('after register',data.Photo[0])
+  const ProfileImage = data.Photo[0]
   registerUser(data.email,data.password)
   .then(result =>{
     console.log(result.user)
+    const formData = new FormData()
+    formData.append('image', ProfileImage)
+
+
+    const image_API_URL=`https://api.imgbb.com/1/upload?&key=${import.meta.env.VITE_image_host_key}`
+
+    axios.post(image_API_URL,formData)
+    .then(res=>{
+      console.log('after image upload', res.data)
+
+    })
   })
   .catch(error=>{
     console.log(error)
@@ -28,6 +41,17 @@ const handleRegistration =(data)=>{
           <label className="label">Email</label>
           <input type="email" {...register('email',{required: true})} className="input" placeholder="Email" />
           {errors.email?.type =='required' &&  <p className='text-red-500'>Email is required</p>}
+
+           <label className="label">Name</label>
+          <input type="text" {...register('name',{required: true})} className="input" placeholder="Your Name" />
+          {errors.name?.type =='required' &&  <p className='text-red-500'>Your Name is required</p>}
+
+
+          <label className="label">Photo</label>
+          <input type="file" {...register('Photo',{required: true})} className="file-input" placeholder="Your Photo" />
+          {errors.name?.type =='required' &&  <p className='text-red-500'>Your Photo is required</p>}
+
+
           <label className="label">Password</label>
           <input type="password" {...register('password',{required: true,
           minLength:6})} className="input" placeholder="Password" />
